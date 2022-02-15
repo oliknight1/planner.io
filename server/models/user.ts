15bff931ecc70cb4e094980354c192016f7dfb0d
Oli Knight <olidknight@gmail.com>
2022-02-15 20:26:27 +0000

    Fix User model typings

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,4 +1,4 @@
-import mongoose, { Types } from 'mongoose';
+import mongoose from 'mongoose';
 import { User } from '../utils/types';
 
 const { Schema } = mongoose;
@@ -7,8 +7,8 @@ export const user_schema = new Schema<User>( {
 	username: { type: String, required: true, index: { unique: true } },
 	password: { type: String, required: true },
 	email: String,
-	projects: [ { type: Types.ObjectId } ],
-	tasks: [ { type: Types.ObjectId } ],
+	projects: [ { type: Schema.Types.ObjectId, ref: 'Project' } ],
+	tasks: [ { type: Schema.Types.ObjectId, ref: 'Task' } ],
 } );
 
 export const UserModel = mongoose.model( 'User', user_schema );
