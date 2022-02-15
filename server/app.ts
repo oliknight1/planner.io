7fcff63a198889892b951eae9bf31531cb4c2c56
Oli Knight <olidknight@gmail.com>
2022-02-15 20:56:31 +0000

    Uncomment database connection

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import { MONGODB_URI } from './utils/config';
 
 export const app = express();
 
 app.use( cors() );
 app.use( express.json() );
 
-// mongoose.connect( config.MONGODB_URI as string )
-//   .then( () => {
-//     console.log( 'Connected to MongoDB database' );
-//   } )
-//   .catch( ( error : Error ) => {
-//     console.error( 'Error connecting to MongoDB', error );
-//   } );
-//
+mongoose.connect( MONGODB_URI as string )
+	.then( () => {
+		console.log( 'Connected to MongoDB database' );
+	} )
+	.catch( ( error : Error ) => {
+		console.error( 'Error connecting to MongoDB', error );
+	} );
