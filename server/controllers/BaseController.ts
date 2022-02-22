460df4bfd24885a2b0df9641581c3335fc3bb89e
Oli Knight <olidknight@gmail.com>
2022-02-22 15:43:41 +0000

    Generalize getting resource by id

--- server/controllers/BaseController.ts
+++ server/controllers/BaseController.ts
@@ -1,19 +1,20 @@
-import mongoose from 'mongoose';
+import { isValidObjectId } from 'mongoose';
 import { Request, Response } from 'express';
-import { UserSchema } from '../utils/types';
+import { User } from '../models/user';
 
 export class BaseController {
-	public static get_one = async (
-		model : mongoose.Model<UserSchema, {}, {}, {}>,
-		search_item : any,
-		request : Request,
-		response : Response,
-	) => {
-		const item = await model.findOne( search_item );
-		if ( item ) {
-			response.status( 200 ).json( item.toJSON() );
+	public static get_by_id = async ( request : Request<{ id: string }>, response : Response ) => {
+		const { id } = request.params;
+
+		if ( !isValidObjectId( id ) ) {
+			response.status( 400 ).json( { error: 'Invalid ID supplied' } );
+		}
+		const user = await User.findById( id );
+
+		if ( user ) {
+			response.status( 200 ).send( user.toJSON() );
 		} else {
-			response.status( 400 ).end();
+			response.status( 404 ).json( { error: 'User not found' } );
 		}
 	};
 }
--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -1,26 +1,11 @@
-import { isValidObjectId } from 'mongoose';
 import { validationResult } from 'express-validator';
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import { User } from '../models/user';
 import { UserSchema } from '../utils/types';
+import { BaseController } from './BaseController';
 
-export class UserController {
-	public static get_by_id = async ( request : Request<{ id: string }>, response : Response ) => {
-		const { id } = request.params;
-
-		if ( !isValidObjectId( id ) ) {
-			response.status( 400 ).json( { error: 'Invalid ID supplied' } );
-		}
-		const user = await User.findById( id );
-
-		if ( user ) {
-			response.status( 200 ).send( user.toJSON() );
-		} else {
-			response.status( 404 ).json( { error: 'User not found' } );
-		}
-	};
-
+export class UserController extends BaseController {
 	public static get_by_email = async (
 		request : Request<{ email: string }>,
 		response : Response,
