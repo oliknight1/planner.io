61139be4d7635b55cfe34411db2f6bd091b27993
Oli Knight <olidknight@gmail.com>
2022-03-05 23:59:56 +0000

    Create basic routes for login / register

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link,
+} from 'react-router-dom';
+import Login from './components/Login';
 import Register from './components/Register';
 
 const App = () => (
-	<Register />
+	<Router>
+		<Routes>
+			<Route path="/register" element={<Register />} />
+			<Route path="/login" element={<Login />} />
+
+		</Routes>
+	</Router>
 );
 
 export default App;
