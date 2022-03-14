1c3d74f0702da0a91d503a96c6276790b76bd7d7
Oli Knight <olidknight@gmail.com>
2022-03-14 20:50:54 +0000

    Add logo to navigaton

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,9 +1,10 @@
 import {
-	Box, Container, IconButton, Image,
+	Box, Container, Flex, IconButton,
 } from '@chakra-ui/react';
 import React, { FC, useState } from 'react';
 import { CloseIcon } from '@chakra-ui/icons';
 import { motion } from 'framer-motion';
+import Logo from './Logo';
 
 const MotionBox = motion( Box );
 
@@ -16,13 +17,18 @@ const Nav : FC = () => {
 	};
 	return (
 		<MotionBox h="100vh" maxW="xs" background="black.900" variants={variants} animate={open ? 'open' : 'closed'}>
-			<Container>
-				<IconButton
-					aria-label="Close navigaton"
-					variant="ghost"
-					icon={<CloseIcon />}
-					onClick={() => set_open( !open )}
-				/>
+			<Container pt={3}>
+				<Flex w="100%" alignItems="center" justifyContent="space-between">
+					<Box w="70%">
+						<Logo />
+					</Box>
+					<IconButton
+						aria-label="Close navigaton"
+						variant="ghost"
+						icon={<CloseIcon w={6} h={6} />}
+						onClick={() => set_open( false )}
+					/>
+				</Flex>
 			</Container>
 		</MotionBox>
 	);
