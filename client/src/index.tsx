a06f655c002a7807bf23cc855313aabe723b6b79
Oli Knight <olidknight@gmail.com>
2022-03-16 19:05:22 +0000

    Add focus visible to remove outline on mouse

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript, CSSReset } from '@chakra-ui/react';
+import { css, Global } from '@emotion/react';
 import theme from './config/theme';
+import 'focus-visible/dist/focus-visible';
 import App from './App';
+
+// Hide focus indicator if clicked by mouse
+const global_styles = css`
+  .js-focus-visible :focus:not([data-focus-visible-added]) {
+     outline: none;
+     box-shadow: none;
+   }
+`;
 
 ReactDOM.render(
 	<ChakraProvider theme={theme}>
 		<React.StrictMode>
+			<Global styles={global_styles} />
 			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+			<CSSReset />
 			<App />
 		</React.StrictMode>
 	</ChakraProvider>,
