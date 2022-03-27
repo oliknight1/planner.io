5dc6e84cd880c9114c8bd981e7d1c081374b9463
Oli Knight <olidknight@gmail.com>
2022-03-27 16:52:52 +0100

    Remove test JWT token

--- server/utils/config.ts
+++ server/utils/config.ts
@@ -2,8 +2,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 export const {
-	PORT, NODE_ENV, JWT_SECRET,
+	PORT, NODE_ENV, JWT_SECRET, JWT_EXPIRES_IN,
 } = process.env;
 
 export const MONGODB_URI = NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
-export const JWT_EXPIRES_IN = NODE_ENV === 'test' ? process.env.TEST_JWT_EXPIRES_IN : process.env.JWT_EXPIRES_IN;
