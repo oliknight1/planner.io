65c6b9c951ca08c86f5fd3c5ba7a77519875dcfd
Oli Knight <olidknight@gmail.com>
2022-04-02 17:47:48 +0100

    Temp removal of view switcher

--- client/src/components/project_page/ProjectTopContent.tsx
+++ client/src/components/project_page/ProjectTopContent.tsx
@@ -4,7 +4,6 @@ import {
 import React, { FC } from 'react';
 import { ViewType } from '../../utils/enums';
 import { is_mobile_breakpoint } from '../../utils/helpers';
-import ViewSwitcher from '../ViewSwitcher';
 
 interface ProjectTopContentProps {
 	title: string,
@@ -15,7 +14,6 @@ const ProjectTopContent : FC<ProjectTopContentProps> = ( { title, set_view, view
 	<>
 		<Flex px={6} py={6} justifyContent="space-between" bg={is_mobile_breakpoint() ? 'black.900' : undefined} maxW="100%">
 			<Heading fontWeight="normal" size="lg">{title}</Heading>
-			<ViewSwitcher view={view} set_view={set_view} />
 		</Flex>
 		<Divider borderColor="white" left={0} width="100%" orientation="horizontal" />
 	</>
