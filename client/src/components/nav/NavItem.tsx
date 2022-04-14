fa08791626b7accba60212976405c3485e213ca2
Oli Knight <olidknight@gmail.com>
2022-04-14 22:38:56 +0100

    Force nav bar to close on nav item click

--- client/src/components/nav/NavItem.tsx
+++ client/src/components/nav/NavItem.tsx
@@ -3,6 +3,8 @@ import {
 } from '@chakra-ui/react';
 import React, { FC } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
+import { useNav } from '../../contexts/nav_context';
+import { is_mobile_breakpoint } from '../../utils/helpers';
 import { NavLink } from '../../utils/types';
 
 const NavItem : FC<NavLink> = ( { name, icon, url } : NavLink ) => {
@@ -11,10 +13,11 @@ const NavItem : FC<NavLink> = ( { name, icon, url } : NavLink ) => {
 		backgroundColor: colorMode === 'dark' ? 'rgba(255,182,0,0.12)' : 'rgba(255,255,255,0.9)',
 		color: colorMode === 'dark' ? 'white' : 'black',
 	};
+	const { toggle_state } = useNav();
 	return (
 		<LinkBox>
 			<Link as={RouterLink} to={url} fontSize="2xl">
-				<ListItem px={2} py={2} _hover={hover_styling} borderRadius="md">
+				<ListItem px={2} py={2} _hover={hover_styling} borderRadius="md" onClick={is_mobile_breakpoint() ? toggle_state : null}>
 					<Wrap align="center" spacing={6}>
 						<WrapItem>
 							{ icon }
